837947590d62828b36975d23972c92d98eb71d4c
DJ McKay <dj@djs-macbook.local>
2017-05-01 14:44:26 -0400

    Cleanup commented code

--- ShoeTraxr/ShoeTableViewController.swift
+++ ShoeTraxr/ShoeTableViewController.swift
@@ -34,7 +34,6 @@ public class ShoeTableViewController: UITableViewController {
         return formatter;
         
     }()
-    //var healthManager:HealthKitManager?
 
     let durationFormatter = DateComponentsFormatter()
     let energyFormatter = EnergyFormatter()
